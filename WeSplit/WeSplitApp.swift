e149141b72e87eb44f177c38303b137a9e836e6b
Neeta Buhecha <neeta.b@gmail.com>
2024-04-24 16:09:32 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Neeta Buhecha on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
