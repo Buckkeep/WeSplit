fff6dc33a4397dc9015be0d8d8095f949d9ad16f
Neeta Buhecha <neeta.b@gmail.com>
2024-04-25 11:59:17 +0100

    Started the app proper. Added text fields with two way binding to accept the user's bill amount.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,34 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
-    @State private var name = ""
-    let students = ["Harry", "Ron", "Hermione", "Neville", "Luna", "Draco"]
-    @State private var selectedStudent = "Harry"
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
         
     var body: some View {
-        
-        NavigationStack{
-            Form {
-                Picker("Choose a Student", selection: $selectedStudent) {
-                    ForEach(students, id:\.self) {
-                        Text($0)
-                    }
-                }
-
-                Section {
-                    Button("Tap count: \(tapCount)") {
-                        tapCount += 1
-                    }
-                }
-                
-                Section {
-                    TextField("Enter your name", text: $name)
-                    Text("Your name is \(name)!")
-                }
-                
-            }.navigationTitle("Select a Student")
-                .navigationBarTitleDisplayMode(.automatic)
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
         }
     }
 }
