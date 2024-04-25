d9ee7e6c49fe5b7be6cc31b3884914414ccd46a2
Neeta Buhecha <neeta.b@gmail.com>
2024-04-25 09:19:08 +0100

    Added a count button, used the State property wrapper and set the affected variable to private to prevent its accidental reuse.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+        
     var body: some View {
+        
         NavigationStack{
             Form {
                 Section {
                     Text("Hello World!")
                     Text("Hello World!")
+                }
+                Button("Tap count: \(tapCount)") {
+                    tapCount += 1
                 }
                 Section {
                     Text("Hello World!")
